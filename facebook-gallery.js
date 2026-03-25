97029b29932950f830374110ab61338373ad5ebd
aboudeh <70956746+aboudeh-hai@users.noreply.github.com>
2026-03-25 15:23:13 +0300

    Update facebook-gallery.js

--- facebook-gallery.js
+++ facebook-gallery.js
@@ -1,43 +1,49 @@
-export class FacebookGallery {
+class FacebookGallery {
   static id = "facebook-gallery";
 
   static isMatch() {
-    return location.href.includes("facebook.com");
+    return window.location.href.includes("facebook.com");
   }
 
+  // per docs, init should return an object (often empty)
   static init() {
-    return new FacebookGallery();
+    return {};
   }
 
   async *run(ctx) {
-    const sleep = ms => new Promise(r => setTimeout(r, ms));
-    const log = msg => console.log("[facebook-gallery]", msg);
+    const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+    const log = (msg) => console.log("[facebook-gallery]", msg);
 
+    // give FB time to render post content
     await sleep(5000);
 
-    const firstImg = document.querySelector("div[role='main'] img");
+    // Try to click something that actually opens the viewer:
+    // clicking <img> often does nothing; click nearest link-like ancestor
+    const img = document.querySelector("div[role='main'] img");
+    const clickable = img?.closest("a, div[role='link'], div[role='button']");
 
-    if (firstImg) {
-      firstImg.click();
-      log("Opened gallery");
+    if (clickable) {
+      clickable.click();
+      log("Opened gallery/lightbox");
       await sleep(3000);
     } else {
-      log("No image found");
+      log("No clickable image found");
+      return;
     }
 
+    // Advance through images in the lightbox
     for (let i = 0; i < 50; i++) {
-      const next = document.querySelector(
-        "[aria-label='Next photo'], [aria-label='Next']"
-      );
+      const next =
+        document.querySelector("[aria-label='Next photo']") ||
+        document.querySelector("[aria-label='Next']");
 
       if (!next) {
-        log("No more images at: " + i);
+        log("No more images at step: " + i);
         break;
       }
 
       next.click();
       log("Clicked next: " + (i + 1));
-
       yield;
       await sleep(2500);
     }
